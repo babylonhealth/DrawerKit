4895d9585ed6265a21306dbbe74a09b2d9f64934
Wagner Truppel <trupwl@gmail.com>
2017-10-11 17:41:27 +0100

    Added tab bar template project

---
+++ DrawerKitDemo/DrawerKitDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DrawerKitDemo
+//
+//  Created by Wagner Truppel on 11/10/2017.
+//  Copyright © 2017 Babylon Health. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
