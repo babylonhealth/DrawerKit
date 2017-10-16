e4fa1c779e32f71adc384b29f864a0143c39e3a7
Wagner Truppel <trupwl@gmail.com>
2017-10-16 12:16:35 +0100

    Added documentation to all exposed public entities.

--- DrawerKit/DrawerKit/Public API/DrawerDisplayController.swift
+++ DrawerKit/DrawerKit/Public API/DrawerDisplayController.swift
@@ -5,14 +5,29 @@ import UIKit
 // - support insufficiently tall content
 // - support not-covering status bar and/or having a gap at the top
 
+/// Instances of this class are returned as part of the `DrawerPresenting` protocol.
 public final class DrawerDisplayController: NSObject {
-    public let configuration: DrawerConfiguration // intentionally immutable
+    /// The collection of configurable parameters dictating how the drawer works.
+    public let configuration: DrawerConfiguration
 
     weak var presentingVC: (UIViewController & DrawerPresenting)?
     /* strong */ var presentedVC: (UIViewController & DrawerPresentable)
 
     let inDebugMode: Bool
 
+    /// Initialiser for `DrawerDisplayController`.
+    ///
+    /// - Parameters:
+    ///   - presentingViewController:
+    ///        the view controller presenting the drawer.
+    ///   - presentedViewController:
+    ///        the view controller wanting to be presented as a drawer.
+    ///   - configuration:
+    ///        the collection of configurable parameters dictating how the drawer works.
+    ///   - inDebugMode:
+    ///        a boolean value which, when true, draws guiding lines on top of the
+    ///        presenting view controller but below the presented view controller.
+    ///        Its default value is false.
     public init(presentingViewController: (UIViewController & DrawerPresenting),
                 presentedViewController: (UIViewController & DrawerPresentable),
                 configuration: DrawerConfiguration = DrawerConfiguration(),
--- DrawerKit/DrawerKit/Public API/DrawerPresenting.swift
+++ DrawerKit/DrawerKit/Public API/DrawerPresenting.swift
@@ -2,5 +2,8 @@ import UIKit
 
 /// Protocol that view controllers presenting a drawer must conform to.
 public protocol DrawerPresenting: class {
+    /// An object vended by the presenting view controller, whose responsibility
+    /// is to coordinate the presentation, animation, and interactivity of/with
+    /// the drawer.
     var drawerDisplayController: DrawerDisplayController? { get }
 }
